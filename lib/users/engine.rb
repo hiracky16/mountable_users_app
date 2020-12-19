68e8edcb7f0fcceea244524f4029672f803a1eeb
hiracky16 <h.piiice16@gmail.com>
2020-12-19 12:42:29 +0900

    user app init

---
+++ lib/users/engine.rb
@@ -0,0 +1,5 @@
+module Users
+  class Engine < ::Rails::Engine
+    config.generators.api_only = true
+  end
+end
---
+++ test/users_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Users::Test < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, Users
+  end
+end
