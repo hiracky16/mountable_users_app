b7cc94186b19b38626c5fc4c45c20ccd38fd5df5
hiracky16 <h.piiice16@gmail.com>
2020-12-19 13:11:30 +0900

    tweet モデル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-User.create(name: 'hiracky16')
+u = User.create(name: 'hiracky16')
+Tweet.create(user: u, content: 'aaa')
+Tweet.create(user: u, content: 'bbb')
+Tweet.create(user: u, content: 'ccc')
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_034359) do
+ActiveRecord::Schema.define(version: 2020_12_19_040203) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
